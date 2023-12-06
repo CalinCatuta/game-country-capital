f71125c15e98b6d453005e9d290f223a75a90c15
CalinCatuta <calinut123@yahoo.co.uk>
2023-12-06 18:54:22 +0200

    add class, sort array and change state.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import { useState } from "react";
+
 const CountryAndCapitalGame = ({ data }) => {
   const countrys = Object.keys(data);
   const capitals = Object.values(data);
-  const options = [...countrys, ...capitals];
+  const [options, setOptions] = useState(
+    [...countrys, ...capitals]
+      .sort(() => Math.random() - 0.5) // Rendomizing the array
+      // this state will have an array with for each keys and value {state: "DEFAULT", value: "Germani"}
+      .map((value) => ({
+        value,
+        state: "DEFAULT",
+      }))
+  );
 
   return (
     <div>
       {options.map((option) => (
-        <button>{option}</button>
+        <button
+          className={option.state === "SELECTED" ? "selected" : ""}
+          onClick={() => {
+            setOptions(
+              options.map((opt) => {
+                // on the option we click on we get that option back in state with the state changed from DEFAULT to Selected
+                return opt === option
+                  ? {
+                      ...opt,
+                      state: "SELECTED",
+                    }
+                  : opt;
+              })
+            );
+          }}
+        >
+          {option.value}
+        </button>
       ))}
     </div>
   );
