44a6ab953555be8b91fd322e82453ac69e906396
CalinCatuta <calinut123@yahoo.co.uk>
2023-12-07 17:39:08 +0200

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { getRandomHexColor } from "./util/RandomColor";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const [color, setColor] = useState(getRandomHexColor);
+  useEffect(() => {
+    setColor(getRandomHexColor);
+  }, []);
+  return (
+    <div>
+      <div className="box" style={{ background: color }}></div>
+      <div className="btns"></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/util/RandomColor.jsx
@@ -0,0 +1,11 @@
+export function getRandomHexColor() {
+  const hexChars = "0123456789ABCDEF";
+  let hexColor = "#";
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * hexChars.length);
+    hexColor += hexChars.charAt(randomIndex);
+  }
+
+  return hexColor;
+}
